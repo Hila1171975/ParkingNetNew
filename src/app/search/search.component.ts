676d09eaaed5b570cdbe1bb783457fa54f98a6c7
Hila <hila1171975@gmail.com>
2022-03-23 22:22:48 +0200

    corrent location

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild, ElementRef, NgZone} from '@angular/core';
 import { Router } from '@angular/router';
 import { Rent } from 'src/classes/Rent';
 import { FindParkingService } from '../Services/find-parking.service';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
+import { MapsAPILoader } from '@agm/core';
+
 
 @Component({
   selector: 'app-search',
@@ -15,31 +17,61 @@ export class SearchComponent implements OnInit {
   TodayEndDate = new Date();
   myLocation: String = "";
   submited = false;
-  lat: number = 22;
-  lan: number = 34;
+  lat!: number;
+  lan!: number ;
+  address!: string;
+  private geoCoder:any;
 
-  constructor(public FindParkingService: FindParkingService, public router: Router) {
+  @ViewChild('search')
+  public searchElementRef: ElementRef| undefined;
+
+  constructor(public FindParkingService: FindParkingService, public router: Router,  private mapsAPILoader: MapsAPILoader,private ngZone: NgZone) {
     this.newRent.EntryDate = this.TodayDate;
     this.newRent.EntryHour = this.TodayDate;
     this.newRent.LeavingDate = this.TodayDate;
     this.newRent.LeavingHour = this.TodayDate;
     this.newRent.LeavingHour?.setHours(0, 0, 0)
+
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+    });
   }
 
   public handleAddressChange(address: Address) {
+    
     this.lat = address.geometry.location.lat()
     debugger;
     this.lan = address.geometry.location.lng()
 
   }
-  ngOnInit(): void {
-
-    console.log(this.TodayDate.getDate);
-
-    //  alert(this.TodayDate.getDate)
+  ngOnInit() {
+   
+  }
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.lat = position.coords.latitude;
+        this.lan = position.coords.longitude;
+        this.getAddress(this.lat, this.lan);
+      });
+    }
   }
 
-
+  getAddress(latitude:number, longitude:number) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results:any, status:any) => {
+      if (status === 'OK') {
+        if (results[0]) {
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+    
+    });
+  }
 
   FindParking() {
     debugger
